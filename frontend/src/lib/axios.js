18c6d786d8edcab7e4b42b9a561a68f51fd7f695
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-05-29 13:30:29 +0530

    ds

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api";
-
+const BASE_URL = "https://baatchit-chat-app.onrender.com/api"
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
