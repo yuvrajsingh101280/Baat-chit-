a37c67404d8ae34e9ce3c12bffb77c6bf781fb50
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-09-16 16:07:54 +0530

    ds

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -53,8 +53,8 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "none", // prevent CSRF attacks
+      secure: true
     });
 
     res.status(201).json({ success: true, user: newUser });
@@ -85,8 +85,8 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "none", // prevent CSRF attacks
+      secure: true
     });
 
     res.status(200).json({ success: true, user });
