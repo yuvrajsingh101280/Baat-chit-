7d57b8f2cb86661101e84af2a74f03d46ade4d17
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-09-16 16:01:14 +0530

    ds

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "https://baat-chit-swmr.vercel.app/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "https://baat-chit-chat.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
