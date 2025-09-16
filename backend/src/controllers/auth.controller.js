6d957525f3bfc0cdac47a2989524932261d992c1
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-09-16 16:18:26 +0530

    ds

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -97,7 +97,11 @@ export async function login(req, res) {
 }
 
 export function logout(req, res) {
-  res.clearCookie("jwt");
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  });
   res.status(200).json({ success: true, message: "Logout successful" });
 }
 
